4fbd065b3c182d5d0299209437d116d2317c4479
al falah taieb <mohamedtaieb.alfalah@esprit.tn>
2017-07-12 13:13:45 +0200

    🤔

--- script.js
+++ script.js
@@ -4,6 +4,43 @@
   recognition.interimResults=true;
 
   let p = document.createElement('p');
-  const word = document.querySelector('.words')
+  const words = document.querySelector('.words')
   words.appendChild(p);
-  
+
+  recognition.addEventListener('result',e=>{
+  	// console.log(e.results)
+  	const transcript=Array.from(e.results)
+  	.map(result=>result[0])
+  	.map(result=>result.transcript)
+  	.join('')
+  	
+  	
+  	p.textContent = transcript;
+ 
+  	if(e.results[0].isFinal){
+  		p=document.createElement('p');
+  		words.appendChild(p);
+  	}
+  	if(transcript.includes('rose')){
+  	    document.body.style.background = "#f3f3f3";
+  	}
+  		if(transcript.includes('blanc')){
+document.body.style.backgroundColor = "#AA0000";
+  	}
+
+if(transcript.includes('Thibault')){
+window.open("https://www.alfelah.com");
+
+  	}
+if(transcript.includes('pensif')){
+	p.textContent=transcript.replace(/pensif|Pensif |chepa/gi, '🤔');
+}
+
+  	console.log(transcript)
+
+  });
+
+recognition.addEventListener('end',recognition.start)
+
+
+  recognition.start();
